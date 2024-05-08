22f06d1e3759d09fc86165eab0370520af4f04fc
fitracandra <enter00##>
2024-05-08 14:27:51 +0700

    perubahan

--- pert1/test.cpp
+++ pert1/test.cpp
@@ -1,6 +1,11 @@
-/// @brief 
-/// @return 
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+// @brief 
+// @return 
 int main()
-{ cout << "hello"; 
+{ 
+    cout << "hello"<<endl; 
 return 0;
  }
