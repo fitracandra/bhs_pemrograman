476cf117bf40ac321f0dcea3e531d3c40d94f38a
fitracandra <enter00##>
2024-05-14 22:36:37 +0700

    first commit

---
+++ latihan/latihan3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+
+    cout<<"Masukkan Angka Pertama : ";
+    cin>>a;
+    cout<<"Masukkan Angka Kedua   : ";
+    cin>>b;
+
+    cout<<"Penjumlahan : "<<a+b<<endl;
+    cout<<"Pengurangan : "<<a-b<<endl;
+    cout<<"Perkalian   : "<<a*b<<endl;
+    cout<<"Pembagian   : "<<a/b<<endl;
+    return 0;
+}
