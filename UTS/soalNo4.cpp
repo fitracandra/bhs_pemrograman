de2523f1cbe947528636cd0062df03c0638233a4
fitracandra <enter00##>
2024-05-21 10:33:52 +0700

    first commit

---
+++ UTS/soalNo4.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int tahunlahir = 2005;
+    int tahunsekarang = 2024;
+    int usia = tahunsekarang - tahunlahir;
+
+    cout<<"Berarti Usia Kalian sekarang adalah "<<usia<<" Tahun"<<endl;
+    return 0;
+}
